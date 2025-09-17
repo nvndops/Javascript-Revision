103147cdb76977cfea43bab2822983777b3209d8
Naveen Verma <nverma2k@gmail.com>
2025-09-17 14:51:58 +0530

    Commit 2: Practiced variables let/const

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,5 @@
+/* 
+//IMPORT/EXPORT Practice
 import util from "./util.js"; //for default export we can name import like this
 import { token, version as appVersion } from "./util.js"; //for named export we can import like this
 import * as utilData from "./util.js"; //can import as object too and call them
@@ -6,4 +8,17 @@ console.log(util);
 console.log(token);
 console.log(appVersion);
 
-console.log(utilData.default);
+console.log(utilData.default); */
+
+// Variables practice 
+//Value in let can be reassigned 
+let userMessage = "Hello world";
+userMessage = "Hello javascript";
+console.log(userMessage);
+console.log(userMessage);
+
+
+//Value in const can not be reassigned 
+const updatedUserMessage = "Hello world!!!";
+//updatedUserMessage = "Hello javascript!!!" this will throw this error "Uncaught TypeError: Assignment to constant variable."
+console.log(updatedUserMessage);
