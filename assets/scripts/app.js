5d70714890cfde7f906f405eb4d963da51da10d3
Naveen Verma <nverma2k@gmail.com>
2025-09-17 20:31:37 +0530

    Commit 3: Practiced some operators

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,10 +9,10 @@ console.log(token);
 console.log(appVersion);
 
 console.log(utilData.default); */
-
+/* ----------------------------------------------------------------------------------------------------------------------------- */
 // Variables practice 
 //Value in let can be reassigned 
-let userMessage = "Hello world";
+/* let userMessage = "Hello world";
 userMessage = "Hello javascript";
 console.log(userMessage);
 console.log(userMessage);
@@ -21,4 +21,15 @@ console.log(userMessage);
 //Value in const can not be reassigned 
 const updatedUserMessage = "Hello world!!!";
 //updatedUserMessage = "Hello javascript!!!" this will throw this error "Uncaught TypeError: Assignment to constant variable."
-console.log(updatedUserMessage);
+console.log(updatedUserMessage); */
+
+/* ----------------------------------------------------------------------------------------------------------------------------- */
+
+//Operators Practice
+console.log(10 + 5); //addition
+console.log(10 - 5); //subtraction
+console.log(10 * 5); //multiplication
+console.log(10 / 5); //division
+console.log("hello " + "world"); //+ operator can also be used for concatination of strings
+
+/* ----------------------------------------------------------------------------------------------------------------------------- */
